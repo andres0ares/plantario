c09a5caaf7c87a3d4ba76ebbc636b1c81a4df13a
André Soares <55996140+andres0ares@users.noreply.github.com>
2021-05-15 15:12:15 -0700

    Updated plantario.ino with POST resquest working

--- arduino/plantario.ino
+++ arduino/plantario.ino
@@ -1,10 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <stdio.h>
+#include <string.h>
 
 #ifndef STASSID
 #define STASSID "brisa-1123109"  //nome do wifi
-#define STAPSK  "hidiqmzu"  //senha wifi       
+#define STAPSK  "hidiqmzu"  //senha wifi    
+#define LED 13   
 #endif
 
 const char* ssid = STASSID;
@@ -17,6 +20,7 @@ const char fingerprint[] PROGMEM = "ccd3380ed0c69d741c8b01d6d0c91ad6526ff1e3";
 int ilu = 5;
 int umi = 60;
 int temp = 29;
+int reservatorio = 70;
 
 void connectWifi(){
   WiFi.begin(ssid, password);
@@ -63,6 +67,11 @@ void getConfig() {
         DeserializationError err = deserializeJson(doc, line);
         
         if(err) { return; }
+        Serial.println(line);
+        digitalWrite(LED, HIGH);
+        delay(3000);
+        digitalWrite(LED, LOW);
+        delay(1000);
         
         temp = doc["data"]["temp"];
         ilu = doc["data"]["ilu"];
@@ -84,49 +93,77 @@ WiFiClientSecure client;
         return;
       }
 
-          /*
-       POST /post HTTP/1.1
-       Host: postman-echo.com
-       Content-Type: application/json
-       Content-Length: 13
+      int temp1 = 26;
+      int ilu1 = 4;
+      int umi1 = 57;
+      int reservatorio1 = 65;
+
+      char *body = "{\"temp\": 21, \"ilu\": 3, \"umi\": 56, \"reservatorio\": 77}";
       
-       say=Hi&to=Mom
-        
-       */
-    
+      // "{\"temp\": " + String(temp1) + ", \"ilu\": " + String(ilu1) + ", \"umi\": " + String(umi1) + ", \"reservatorio\": " + String(reservatorio1) + "}"
+      // "{\"temp\": 21, \"ilu\": 3, \"umi\": 56, \"reservatorio\": 77}" 
+      // "{"temp": 21, "ilu": 3, "umi": 56, "reservatorio": 77}" 
+
+      //snprintf(body, sizeof body, "{\"temp\": %d, \"ilu\": %d, \"umi\": %d, \"reservatorio\": %d}", temp1, ilu1, umi1, reservatorio1);
+
+      Serial.println(body);
+      Serial.println(strlen(body));
+      
       String url = "/api/report";
       client.print(String("POST ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "User-Agent: BuildFailureDetectorESP8266\r\n" +
                "Content-Type: application/json"+ "\r\n" +
-               "{\"temp\":28,\"ilu\":7,\"umi\":78}" + "\r\n" +
+               "Content-Length: "+ strlen(body) + "\r\n\r\n" + body + "\r\n" +
                "Connection: close\r\n\r\n");
 
       //solicitacao enviada
+
+      
       
       while (client.connected()) {
         String line = client.readStringUntil('\n');
         if (line == "\r") {
-          //headers received
+          Serial.println(line);
           break;
         }
+      }
+
+      String line = client.readStringUntil('\n');
+      if (line.startsWith("{\"success\":true")) {
+          Serial.println(line);
+          digitalWrite(LED, HIGH);
+          delay(5000);
+          digitalWrite(LED, LOW);
+          delay(1000);      
+      }else if(line.startsWith("{\"success\":false")) {
+          Serial.println(line);
+          digitalWrite(LED, HIGH);
+          delay(7000);
+          digitalWrite(LED, LOW);
+          delay(1000);      
       }
 
 }
 
 void setup() {
   // put your setup code here, to run once:
-  
 
-  Serial.begin(115200);
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
   connectWifi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
- 
-      
+
+    
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    
     getConfig();
     sendReport();  
     delay(10000);
