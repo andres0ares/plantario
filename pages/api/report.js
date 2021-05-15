8956d69c68315237a88302b9c8760e72f5de1993
André Soares <55996140+andres0ares@users.noreply.github.com>
2021-05-15 13:28:05 -0700

    Updated error feedback

--- pages/api/report.js
+++ pages/api/report.js
@@ -60,7 +60,7 @@ export default async (req, res)  => {
                 
             } catch (error) {
                 console.log(error)
-                res.status(400).json({success: false})
+                res.status(400).json({success: false, error: error})
             }
             break
         default:
