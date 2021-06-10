79f0bd3398aaa8bb5aaab6a6ac90a99bbae8937c
Andre Soares <andreiarley@gmail.com>
2021-06-10 17:20:07 -0300

    fixing bud not sending email 2

--- pages/api/report.js
+++ pages/api/report.js
@@ -1,7 +1,7 @@
 import Report from '../../utils/models/Report'
 import Command from '../../utils/models/Command'
 import dbConnect from '../../utils/dbConnect'
-import sendemail from '../../utils/sendemail'
+//import sendemail from '../../utils/sendemail'
 
 dbConnect()
 
@@ -116,4 +116,39 @@ export default async (req, res)  => {
             break
     }
     
+}
+
+
+
+"use strict"
+import nodemailer from 'nodemailer'
+const { SENDEMAIL_EMAIL, SENDEMAIL_PASSWORD } = process.env
+
+
+async function sendmail() {
+ 
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp-relay.sendinblue.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: SENDEMAIL_EMAIL, 
+      pass: SENDEMAIL_PASSWORD, 
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Plantário 🌱" <reservatorio@plantario.oandre.com>', // sender address
+    to: "andreiarley@gmail.com", // list of receivers
+    subject: "Alerta reservatório", // Subject line
+    text: "Passando aqui para lembrar que o reservatório está abaixo de sua capacidade. Reabasteça o quanto antes! ", // plain text body
+    html: "<b>Passando aqui para lembrar que o reservatório está abaixo de sua capacidade. Reabasteça o quanto antes! </b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
 }
